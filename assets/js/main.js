1366cf06e3982e79940816b143bd76e86f9f7ac3
diego-cavenati <diegocavenati@gmail.com>
2022-10-26 13:45:03 +0200

    Say hello word

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data
+
+
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      title: "Hello World!"
+    }
+  }
+}).mount('#app')
